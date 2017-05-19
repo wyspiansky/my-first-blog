9326c1f6382b01646a39210bf3e2e72cedc59b00
admin <wyspiansky@gmail.com>
2017-05-19 12:05:53 +0200

    Zmieniony kod HTML na stronie.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9k#8e$viqiuwzfh37^o+3ggx$0x@694&=&l_r37w0sfjv6_bjs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
